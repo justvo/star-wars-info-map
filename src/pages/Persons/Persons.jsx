d24c901f8d539c17d7783a1f9c8598d1e65546ca
justvo <vlad.otyan.02@gmail.com>
2024-04-03 09:53:55 +0300

    fix/ the component is formatted

--- src/pages/Persons/Persons.jsx
+++ src/pages/Persons/Persons.jsx
@@ -1,17 +1,11 @@
-import { useSelector, useDispatch } from "react-redux";
 import PersonItem from "../../components/PersonItem";
-import { useState } from "react";
-import ModalWin from "../../components/ModalWin";
 
-const Persons =()=>{
-
-
-
-    return (
-        <div>
-            <PersonItem/>
-        </div>
-    )
-}
+const Persons = () => {
+  return (
+    <div>
+      <PersonItem />
+    </div>
+  );
+};
 
 export default Persons;
